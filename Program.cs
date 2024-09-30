a577f658d3e1ba6107e6d0ccfdad445b2003d3cc
Erick Vinicius <viniciuserick1029@gmail.com>
2024-09-29 21:12:20 -0300

    Completed Task

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia("12345", "Nokia 3310", "111111111", 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Smartphone iphone = new Iphone("67890", "iPhone 14", "222222222", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
